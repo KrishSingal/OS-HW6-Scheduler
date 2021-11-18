275e3eb4c068eea3dc5f3d9d18c4c3f707deac78
Nikhil Mehta <nm3077@columbia.edu>
2021-11-18 01:02:42 -0500

    checkpatch clean, readme, updated part3 responses

--- part4/test.c
+++ part4/test.c
@@ -9,27 +9,27 @@
 
 
 
-int main(void) 
+int main(void)
 {
 	pid_t pid[5];
+
 	for (int i = 0; i < 5; i++) {
 		pid[i] = fork();
 		if (pid[i] == 0) {
 			cpu_set_t mask;
+
 			CPU_ZERO(&mask);
 			CPU_SET(3, &mask);
 			sched_setaffinity(pid[i], sizeof(mask), &mask);
 			for (int j = 0; j < 100000; j++) {
-				for (int k = 0; k < 100000; k++) {
+				for (int k = 0; k < 100000; k++)
 					;
-				}
 			}
 			printf("child done\n");
 			return 0;
 		}
 	}
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 5; i++)
 		waitpid(pid[i], NULL, 0);
-	}
 	printf("done\n");
 }
