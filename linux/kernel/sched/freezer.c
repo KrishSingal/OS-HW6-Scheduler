e2013b2d00cdeb4d547d9a741c5432181d849ad1
Nikhil Mehta <nm3077@columbia.edu>
2021-11-20 17:44:05 -0500

    part 7 worksgit add user/test/set-freezer/set-freezer.c removed print statements, checkpatch clean!

--- linux/kernel/sched/freezer.c
+++ linux/kernel/sched/freezer.c
@@ -1,5 +1,4 @@
 #include "sched.h"
-#include <linux/printk.h>
 
 /*
  * freezer scheduling policy implementation
@@ -11,7 +10,7 @@ static void update_curr_freezer(struct rq *rq)
 	// struct sched_freezer_entity *fr_se = &curr->fr;
 	u64 delta_exec;
 	u64 now;
-	pr_info("Update curr freezer");
+
 	now = rq_clock_task(rq);
 	delta_exec = now - curr->se.exec_start;
 	if (unlikely((s64)delta_exec <= 0))
@@ -22,19 +21,17 @@ static void update_curr_freezer(struct rq *rq)
 	curr->se.exec_start = now;
 }
 
-static void 
+static void
 enqueue_task_freezer(struct rq *rq, struct task_struct *p, int flags)
 {
-	pr_info("Enqueue task freezer");
 	list_add_tail(&p->fr.run_list, &rq->fr.active);
 	rq->fr.fr_nr_running++;
 	p->fr.on_rq = 1;
 }
 
-static void 
+static void
 dequeue_task_freezer(struct rq *rq, struct task_struct *p, int flags)
 {
-	pr_info("Dequeue task freezer");
 	update_curr_freezer(rq);
 	list_del(&p->fr.run_list);
 	rq->fr.fr_nr_running--;
@@ -43,15 +40,13 @@ dequeue_task_freezer(struct rq *rq, struct task_struct *p, int flags)
 
 static void yield_task_freezer(struct rq *rq)
 {
-	pr_info("Yield task freezer");
-	list_move_tail(&rq->curr->fr.run_list, &rq->fr.active);	
+	list_move_tail(&rq->curr->fr.run_list, &rq->fr.active);
 	update_curr_freezer(rq);
 }
 
-static void 
+static void
 check_preempt_curr_freezer(struct rq *rq, struct task_struct *p, int flags)
 {
-	pr_info("Check preempt curr task freezer");
 	//Don't need to reschedule since we dont deal with priority
 }
 
@@ -60,10 +55,9 @@ static struct task_struct *pick_next_task_freezer(struct rq *rq)
 	struct sched_freezer_entity *fr_se;
 	struct task_struct *p;
 
-	pr_info("Pick next task freezer");
 	if (rq->fr.fr_nr_running == 0)
 		return NULL;
-	
+
 	fr_se = list_first_entry(&rq->fr.active, struct sched_freezer_entity, run_list);
 	p = container_of(fr_se, struct task_struct, fr);
 	p->se.exec_start = rq_clock_task(rq);
@@ -72,23 +66,20 @@ static struct task_struct *pick_next_task_freezer(struct rq *rq)
 
 static void put_prev_task_freezer(struct rq *rq, struct task_struct *p)
 {
-	pr_info("Put prev task freezer");
 	update_curr_freezer(rq);
 }
 
-static void 
+static void
 set_next_task_freezer(struct rq *rq, struct task_struct *p, bool first)
 {
-	pr_info("Set next task freezer");
 	p->se.exec_start = rq_clock_task(rq);
 }
 
 #ifdef CONFIG_SMP
-static int 
+static int
 balance_freezer(struct rq *rq, struct task_struct *p, struct rq_flags *rf)
 {
-	pr_info("Balance freezer");
-	return sched_stop_runnable(rq) || sched_dl_runnable(rq) || 
+	return sched_stop_runnable(rq) || sched_dl_runnable(rq) ||
 		sched_rt_runnable(rq) || rq->fr.fr_nr_running > 0;
 }
 
@@ -98,9 +89,10 @@ select_task_rq_freezer(struct task_struct *p, int cpu, int sd_flag, int flags)
 	int i;
 	int min_cpu = task_cpu(p);
 	int min = ((int)(~0U >> 1)); //MAX INT
-	pr_info("Select task rq freezer");
+
 	for_each_cpu(i, p->cpus_ptr) {
 		struct rq *rq = cpu_rq(i);
+
 		if (rq->fr.fr_nr_running < min) {
 			min = rq->fr.fr_nr_running;
 			min_cpu = i;
@@ -112,7 +104,6 @@ select_task_rq_freezer(struct task_struct *p, int cpu, int sd_flag, int flags)
 
 static void task_tick_freezer(struct rq *rq, struct task_struct *p, int queued)
 {
-	pr_info("Task tick freezer");
 	update_curr_freezer(rq);
 
 	if (--p->fr.time_slice)
@@ -122,44 +113,40 @@ static void task_tick_freezer(struct rq *rq, struct task_struct *p, int queued)
 
 	//Move to back of list if not only element
 	if (p->fr.run_list.prev != p->fr.run_list.next) {
-			list_move_tail(&p->fr.run_list, &rq->fr.active);	
-			resched_curr(rq);
-			return;
+		list_move_tail(&p->fr.run_list, &rq->fr.active);
+		resched_curr(rq);
+		return;
 	}
 }
 
 static void switched_from_freezer(struct rq *rq, struct task_struct *p)
 {
-	pr_info("Switched from freezer");
 	//Do we need?
 }
 
 static void switched_to_freezer(struct rq *rq, struct task_struct *p)
 {
-	pr_info("Switched to freezer");
 	//No Op?
 }
 
 static void
 prio_changed_freezer(struct rq *rq, struct task_struct *p, int oldprio)
 {
-	pr_info("Prio changed freezer");
 	//No Op?
 }
 
-static unsigned int 
+static unsigned int
 get_rr_interval_freezer(struct rq *rq, struct task_struct *task)
 {
-	pr_info("Get rr interval freezer");
 	return FREEZER_TIMESLICE;
 }
 
-const struct sched_class freezer_sched_class 
+const struct sched_class freezer_sched_class
 	__section("__freezer_sched_class") = {
 	.enqueue_task = enqueue_task_freezer,
 	.dequeue_task = dequeue_task_freezer,
 	.yield_task = yield_task_freezer,
-	
+
 	.check_preempt_curr = check_preempt_curr_freezer,
 
 	.pick_next_task = pick_next_task_freezer,
@@ -169,24 +156,25 @@ const struct sched_class freezer_sched_class
 
 #ifdef CONFIG_SMP
 
-	.balance = balance_freezer, 
+	.balance = balance_freezer,
 	.select_task_rq = select_task_rq_freezer,
 
 	.set_cpus_allowed = set_cpus_allowed_common,
 #endif
 
 	.task_tick = task_tick_freezer,
-	.switched_from = switched_from_freezer, 
+	.switched_from = switched_from_freezer,
 	.switched_to = switched_to_freezer,
 	.prio_changed = prio_changed_freezer,
 
 	.get_rr_interval = get_rr_interval_freezer,
 
 	.update_curr = update_curr_freezer,
-	
+
 };
 
-void init_fr_rq(struct freezer_rq *fr_rq){
+void init_fr_rq(struct freezer_rq *fr_rq)
+{
 	INIT_LIST_HEAD(&fr_rq->active);
 	fr_rq->fr_nr_running = 0;
 }
--- user/test/set-freezer/set-freezer.c
+++ user/test/set-freezer/set-freezer.c
@@ -4,13 +4,15 @@
 
 struct sched_param sp;
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	if (argc != 2) {
 		printf("please give PID to change to freezer");
 		return -1;
 	}
 	int pid = atoi(argv[1]);
 	int num = sched_getscheduler(pid);
+
 	sp.sched_priority = 0;
 	sched_setscheduler(pid, 7, &sp);
 	printf("[%d] sched policy changed: %d --> 7\n", pid, num);
