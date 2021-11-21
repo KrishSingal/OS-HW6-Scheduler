39be119c9ac9358aabb619f53e2881ec34d5f800
Nikhil Mehta <nm3077@columbia.edu>
2021-11-20 23:30:25 -0500

    fixed else case for rt_mutex...

--- linux/kernel/sched/core.c
+++ linux/kernel/sched/core.c
@@ -4933,7 +4933,10 @@ void rt_mutex_setprio(struct task_struct *p, struct task_struct *pi_task)
 			p->dl.pi_se = &p->dl;
 		if (rt_prio(oldprio))
 			p->rt.timeout = 0;
-		p->sched_class = &freezer_sched_class;
+		if (p->policy == SCHED_FREEZER)
+			p->sched_class = &freezer_sched_class;
+		else
+			p->sched_class = &fair_sched_class;
 	}
 
 	p->prio = prio;
